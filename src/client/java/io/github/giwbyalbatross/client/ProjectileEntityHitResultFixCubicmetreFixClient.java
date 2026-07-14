779d2a6c6922e0d5b0ae41ddae207b481439aaa8
GiwbyAlbatross <giwby.albatross@gmail.com>
2026-07-14 18:09:18 +1000

    sorta works sometimes? it's buggy as hell though and needs work

---
+++ src/client/java/io/github/giwbyalbatross/client/ProjectileEntityHitResultFixCubicmetreFixClient.java
@@ -0,0 +1,10 @@
+package io.github.giwbyalbatross.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ProjectileEntityHitResultFixCubicmetreFixClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/io/github/giwbyalbatross/mixin/AbstractArrowMixin.java
@@ -0,0 +1,46 @@
+package io.github.giwbyalbatross.mixin;
+
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.projectile.ProjectileUtil;
+import net.minecraft.world.entity.projectile.arrow.AbstractArrow;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.EntityHitResult;
+import net.minecraft.world.phys.Vec3;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.jspecify.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(AbstractArrow.class)
+public abstract class AbstractArrowMixin extends AbstractArrow {
+	public AbstractArrowMixin(
+      final EntityType<? extends AbstractArrow> type,
+      final LivingEntity mob,
+      final Level level,
+      final ItemStack pickupItemStack,
+      final @Nullable ItemStack firedFromWeapon
+   ) {
+      super(type, mob.getX(), mob.getEyeY() - 0.1F, mob.getZ(), level, pickupItemStack, firedFromWeapon);
+      this.setOwner(mob);
+   }
+
+	@Inject(at = @At("HEAD"), method = "findHitEntities")
+	private Collection<EntityHitResult> findHitEntitiesOverride(final Vec3 from, final Vec3 to, CallbackInfo info) {
+		EntityHitResult ehr =  ProjectileUtil.getEntityHitResult(
+        	 this.level(), this, from, to, this.getBoundingBox().expandTowards(this.getDeltaMovement()).inflate(1.0), this::canHitEntity
+      	);
+		List<EntityHitResult> r = new ArrayList<EntityHitResult>();
+
+		r.add(ehr);
+
+		return r;
+	}
+}
