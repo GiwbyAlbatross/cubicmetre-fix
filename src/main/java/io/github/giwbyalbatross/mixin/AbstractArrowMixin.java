9ea605cbc659275cd7cb3825a118f449875ac565
Giwby Albatross <giwby.albatross@gmail.com>
2026-07-15 07:38:18 +1000

    a fix for the fix
    ALSO UNTESTED by hey we got github actions testing stuff for us :)

--- src/main/java/io/github/giwbyalbatross/mixin/AbstractArrowMixin.java
+++ src/main/java/io/github/giwbyalbatross/mixin/AbstractArrowMixin.java
@@ -17,7 +17,7 @@ import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(AbstractArrow.class)
 public abstract class AbstractArrowMixin extends AbstractArrow {
